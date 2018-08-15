f712cc43e044cce84ce45e8bc3f6657c82a5dd37
Sujan Mahmud <sujancseruet@gmail.com>
2018-08-15 19:51:01 +0600

    Composer added

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,15 +5,11 @@
  *
  * @var array
  */
+require 'vendor/autoload.php';
 $app = [];
 
 require 'core/functions.php';
 
 $app['config'] = require 'config.php';
-
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/QueryBuilder.php';
-require 'core/database/Connection.php';
 
 $app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
